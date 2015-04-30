55d2666aa72fff834d651e59223a76cbf181a226
junhui <junhui@cjh>
2015-04-30 17:07:05 +0800

    i am fork of test2 by cjh-test

---
+++ testforgit/src/testforgit/Testforgit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testforgit;
+
+/**
+ *
+ * @author junhui
+ */
+public class Testforgit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String str="i am fork of test2 by cjh-test";
+    }
+    
+}
